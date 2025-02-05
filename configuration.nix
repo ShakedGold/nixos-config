9b0a5e3da77abd8eceb5e9302a8f29801e3770fb
Shaked Gold <shakedgold2005@gmail.com>
2025-02-05 11:27:19 +0200

    170 current  2025-02-05 11:27:16  24.11.714063.fecfeb863283  6.6.75                          * - Wed 05 Feb 2025 11:27:19 IST

--- configuration.nix
+++ configuration.nix
@@ -307,49 +307,6 @@
     kdePackages.bluedevil
   ];
 
-  # environment for ulauncher
-  systemd.user.services.ulauncher = {
-    enable = true;
-    after = ["network.target"];
-    wantedBy = ["default.target"];
-
-    unitConfig = {
-      "Description" = "Linux Application Launcher";
-      "Documentation" = ["https://ulauncher.io/"];
-    };
-
-    environment = let
-      pydeps = pkgs.python3.withPackages (pp:
-        with pp; [
-          # calculate anything
-          google
-          pytz # https://github.com/tchar/ulauncher-albert-calculate-anything
-          pint # https://github.com/tchar/ulauncher-albert-calculate-anything
-          simpleeval # https://github.com/tchar/ulauncher-albert-calculate-anything
-          requests # https://github.com/tchar/ulauncher-albert-calculate-anything
-          parsedatetime # https://github.com/tchar/ulauncher-albert-calculate-anything
-          google-api-python-client # https://github.com/Carlosmape/ulauncher-calendar/blob/master/requirements.txt
-          google-auth-oauthlib # https://github.com/Carlosmape/ulauncher-calendar/blob/master/requirements.txt
-          pydbus
-          pygobject3
-
-          # spotify webapi
-          spotipy
-        ]);
-    in {
-      PYTHONPATH = "${pydeps}/${pydeps.sitePackages}";
-    };
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = 1;
-      ExecStart = pkgs.writeShellScript "ulauncher-env-wrapper.sh" ''
-        export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-        exec ${pkgs.ulauncher}/bin/ulauncher --hide-window --verbose
-      '';
-    };
-  };
-
   # add directx -> vulcan
   nixpkgs.overlays = [
     (self: super: {
--- home.nix
+++ home.nix
@@ -110,6 +110,49 @@ in {
     enable = true;
   };
 
+  # environment for ulauncher
+  systemd.user.services.ulauncher = {
+    enable = true;
+    after = ["network.target"];
+    wantedBy = ["default.target"];
+
+    unitConfig = {
+      "Description" = "Linux Application Launcher";
+      "Documentation" = ["https://ulauncher.io/"];
+    };
+
+    environment = let
+      pydeps = pkgs.python3.withPackages (pp:
+        with pp; [
+          # calculate anything
+          google
+          pytz # https://github.com/tchar/ulauncher-albert-calculate-anything
+          pint # https://github.com/tchar/ulauncher-albert-calculate-anything
+          simpleeval # https://github.com/tchar/ulauncher-albert-calculate-anything
+          requests # https://github.com/tchar/ulauncher-albert-calculate-anything
+          parsedatetime # https://github.com/tchar/ulauncher-albert-calculate-anything
+          google-api-python-client # https://github.com/Carlosmape/ulauncher-calendar/blob/master/requirements.txt
+          google-auth-oauthlib # https://github.com/Carlosmape/ulauncher-calendar/blob/master/requirements.txt
+          pydbus
+          pygobject3
+
+          # spotify webapi
+          spotipy
+        ]);
+    in {
+      PYTHONPATH = "${pydeps}/${pydeps.sitePackages}";
+    };
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = 1;
+      ExecStart = pkgs.writeShellScript "ulauncher-env-wrapper.sh" ''
+        export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+        exec ${pkgs.ulauncher}/bin/ulauncher --hide-window --verbose
+      '';
+    };
+  };
+
   # Programs
   programs.git = {
     enable = true;
@@ -289,6 +332,15 @@ in {
           accelerationProfile = "none";
           scrollSpeed = 1;
           vendorId = "1038";
+          productId = "183a";
+        }
+        {
+          enable = true;
+          name = "Aerox 3";
+          acceleration = 0;
+          accelerationProfile = "none";
+          scrollSpeed = 1;
+          vendorId = "0111";
           productId = "183a";
         }
       ];
