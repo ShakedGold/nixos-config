8e383c42e701ae5be272278fc186a355cbf5637d
Shaked Gold <shakedgold2005@gmail.com>
2025-03-22 23:11:36 +0200

    home manager obs

--- home.nix
+++ home.nix
@@ -122,8 +122,14 @@ in {
     enable = true;
   };
 
-  programs.obs-studio.package = inputs.nixpkgs-obs-nvenc.legacyPackages.${pkgs.system}.obs-studio.override {
-    cudaSupport = true;
+  programs.obs-studio = {
+    enable = true;
+    package = inputs.nixpkgs-obs-nvenc.legacyPackages.${pkgs.system}.obs-studio.override {
+      cudaSupport = true;
+    };
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-vaapi
+    ];
   };
 
   # environment for ulauncher
