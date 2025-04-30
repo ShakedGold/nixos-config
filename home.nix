90e7b00d3e25c56c5f37e1fc3003d9b6583a1805
Shaked Gold <shakedgold2005@gmail.com>
2025-04-30 18:55:26 +0300

    238 current  2025-04-18 15:57:15  25.05.20250322.1e5b653  6.13.7-zen1                          * - Wed 30 Apr 2025 18:55:26 IDT

--- home.nix
+++ home.nix
@@ -99,19 +99,6 @@ in {
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-
-  wayland.windowManager.hyprland = {
-    # Whether to enable Hyprland wayland compositor
-    enable = true;
-    # The hyprland package to use
-    package = pkgs.hyprland;
-    # Whether to enable XWayland
-    xwayland.enable = true;
-
-    # Optional
-    # Whether to enable hyprland-session.target on hyprland startup
-    systemd.enable = true;
-  };
   
   xdg.desktopEntries = {
     "1password" = {
