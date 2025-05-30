45bd0de3d13360f8841eaf6755c07f0beb2148c0
Shaked Gold <shakedgold2005@gmail.com>
2025-05-30 13:39:59 +0300

    441 current  2025-05-30 13:35:09  25.11.20250520.2795c50  6.14.7-zen1                          * - Fri 30 May 2025 13:39:59 IDT

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
