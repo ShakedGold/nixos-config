620d376d7a46c1d41cc8fa93b9db8911635a6912
Shaked Gold <shakedgold2005@gmail.com>
2025-06-16 09:52:24 +0300

    - Mon 16 Jun 2025 09:52:24 IDT

--- configuration.nix
+++ configuration.nix
@@ -175,7 +175,7 @@
 
   # Enable sound with pipewire.
   security.rtkit.enable = true;
-  services.pulseaudio.enable = false;
+  # services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
