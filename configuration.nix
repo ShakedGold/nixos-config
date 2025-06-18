98f3e5aa18c18ae9625a5d844b1f3faa9e20db8a
Shaked Gold <shakedgold2005@gmail.com>
2025-06-18 19:24:13 +0300

    - Wed 18 Jun 2025 19:24:13 IDT

--- configuration.nix
+++ configuration.nix
@@ -263,6 +263,14 @@
     ];
   };
 
+  xdg.mime.defaultApplications = {
+      "text/html" = "/run/current-system/sw/share/applications/firefox.desktop";
+      "x-scheme-handler/http" = "/run/current-system/sw/share/applications/firefox.desktop";
+      "x-scheme-handler/https" = "/run/current-system/sw/share/applications/firefox.desktop";
+      "x-scheme-handler/about" = "/run/current-system/sw/share/applications/firefox.desktop";
+      "x-scheme-handler/unknown" = "/run/current-system/sw/share/applications/firefox.desktop";
+  };
+
   services.tailscale.enable = true;
   services.tailscale.useRoutingFeatures = "client";
 
