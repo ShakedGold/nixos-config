47654c3adf781338856af8323313c6e245ff5f40
Shaked Gold <shakedgold2005@gmail.com>
2025-06-20 14:57:05 +0300

    - Fri 20 Jun 2025 14:57:05 IDT

--- configuration.nix
+++ configuration.nix
@@ -117,11 +117,6 @@
     xwayland.enable = true;
   };
 
-  environment.sessionVariables = {
-    # NIXOS_OZONE_WL = "1";
-    # XDG_SESSION_TYPE="wayland";
-  };
-
   environment.etc = {
     "1password/custom_allowed_browsers" = {
       text = ''
@@ -396,6 +391,25 @@
     };
     package = pkgs.kanata-with-cmd;
   };
+
+      systemd.user.services."wait-for-full-path" = {
+      description = "wait for systemd units to have full PATH";
+      wantedBy = [ "xdg-desktop-portal.service" ];
+      before = [ "xdg-desktop-portal.service" ];
+      path = with pkgs; [ systemd coreutils gnugrep ];
+      script = ''
+        ispresent () {
+          systemctl --user show-environment | grep -E '^PATH=.*/.nix-profile/bin'
+        }
+        while ! ispresent; do
+          sleep 0.1;
+        done
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        TimeoutStartSec = "60";
+      };
+    };
 
 
   # Some programs need SUID wrappers, can be configured further or are
