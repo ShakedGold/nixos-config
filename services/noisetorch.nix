55c995c791fd22f5805e12fac06ca82bd4a1b3fb
Shaked Gold <shakedgold2005@gmail.com>
2025-06-23 10:17:19 +0300

    - Mon 23 Jun 2025 10:17:19 IDT

--- services/noisetorch.nix
+++ services/noisetorch.nix
@@ -17,7 +17,7 @@
     Service = {
       Type = "simple";
       Restart = "always";
-      RestartSec = 1;
+      RestartSec = 30;
       ExecStart = pkgs.writeShellScript "noisetorch.sh" ''
       noisetorch -u
       noisetorch -i -s alsa_output.usb-SteelSeries_SteelSeries_GameDAC_000000000000-00.stereo-chat.monitor
