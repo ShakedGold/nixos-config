5331d51e3df149805f7084e3ef027b21447a9fb2
Shaked Gold <shakedgold2005@gmail.com>
2025-06-23 11:30:56 +0300

    - Mon 23 Jun 2025 11:30:56 IDT

--- services/albert.nix
+++ services/albert.nix
@@ -2,7 +2,7 @@
   pkgs,
   ...
 } : {
-  systemd.user.services.ulauncher = {
+  systemd.user.services.albert = {
     Install = {
       WantedBy = ["default.target"];
     };
@@ -34,7 +34,7 @@
     in
     {
       Type = "simple";
-      Restart = "always";
+      Restart = "unless-stopped";
       RestartSec = 1;
       ExecStart = pkgs.writeShellScript "albert-env-wrapper.sh" ''
         export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
