7df4486362a378d68c825cfa2b9bba5f8245326d
Shaked Gold <shakedgold2005@gmail.com>
2025-06-23 11:01:20 +0300

    - Mon 23 Jun 2025 11:01:20 IDT

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -58,9 +58,9 @@ in {
     };
 
     envExtra = ''
-      ZSH_TMUX_AUTOSTART=true
-      ZSH_TMUX_FIXTERM_WITH_256COLOR=true
-      ZSH_TMUX_UNICODE=true
+      # ZSH_TMUX_AUTOSTART=true
+      # ZSH_TMUX_FIXTERM_WITH_256COLOR=true
+      # ZSH_TMUX_UNICODE=true
     '';
 
     initContent = ''
