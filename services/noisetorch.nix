1a2675d3233aca48d8f74e64da53cff3c8ccbc23
Shaked Gold <shakedgold2005@gmail.com>
2025-06-23 10:13:30 +0300

    - Mon 23 Jun 2025 10:13:30 IDT

---
+++ services/noisetorch.nix
@@ -0,0 +1,24 @@
+{
+  pkgs,
+  ...
+}
+:
+{
+  systemd.user.services.noisetorch = {
+    Install = {
+      WantedBy = ["default.target"];
+    };
+
+    Unit = {
+      "Description" = "Voice reduction software";
+      After = ["network.target"];
+    };
+
+    Service = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = 1;
+      ExecStart = pkgs.writeShellScript "noisetorch.sh" ''noisetorch -i -s alsa_output.usb-SteelSeries_SteelSeries_GameDAC_000000000000-00.stereo-chat.monitor'';
+    };
+  };
+}
