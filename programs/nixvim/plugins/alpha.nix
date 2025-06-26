c5119a6a955a4222f5baaa16233c009d36e5df25
Shaked Gold <shakedgold2005@gmail.com>
2025-06-27 01:55:18 +0300

    - Fri 27 Jun 2025 01:55:18 IDT

--- programs/nixvim/plugins/alpha.nix
+++ programs/nixvim/plugins/alpha.nix
@@ -2,8 +2,11 @@
   programs.nixvim = {
     plugins.alpha = {
       enable = true;
-      theme = "dashboard";
       layout = [
+        {
+          type = "padding";
+          val = 2;
+        }
         {
           opts = {
             hl = "Type";
@@ -22,6 +25,47 @@
             " ██████  █████████████████████ ████ █████ █████ ████ ██████ "
             "                                                                       "
           ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          type = "group";
+          val = [
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[ene]] end";
+              };
+              opts = {
+                shortcut = "n";
+              };
+              type = "button";
+              val = "  New file";
+            }
+            {
+              on_press = {
+                __raw = "function() vim.cmd[[qa]] end";
+              };
+              opts = {
+                shortcut = "q";
+              };
+              type = "button";
+              val = " Quit Neovim";
+            }
+          ];
+        }
+        {
+          type = "padding";
+          val = 2;
+        }
+        {
+          opts = {
+            hl = "Keyword";
+            position = "center";
+          };
+          type = "text";
+          val = "Inspiring quote here.";
         }
       ];
     };
