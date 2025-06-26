129bda92293c9db57dad1eb12cbebce43ce0f04a
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 22:20:30 +0300

    - Thu 26 Jun 2025 22:20:30 IDT

--- programs/nixvim/options.nix
+++ programs/nixvim/options.nix
@@ -4,10 +4,13 @@
     enable = true;
 
     opts = {
+      expandtab = true;
       number = true;         # Show line numbers
       relativenumber = true; # Show relative line numbers
+      tabstop = 4;
+      softtabstop = 4;
 
-      shiftwidth = 2;        # Tab width should be 2
+      shiftwidth = 4;        # Tab width should be 2
     };
   };
 }
