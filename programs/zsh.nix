b405f79eab1e5c9b16b1e20f08a5f746403fcef5
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 16:48:20 +0300

    - Thu 26 Jun 2025 16:48:20 IDT

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -9,7 +9,7 @@
       local nix_prompt=""
       if [ -n "$IN_NIX_SHELL" ]; then
         # Nerd Font icon U+F313 with text
-        nix_prompt="%{$fg[red]%}[[%{$fg_bold[white]%}nix-shell  {$reset_color%}%{$fg[red]%}]]"
+        nix_prompt="%{$fg[red]%}[%{$fg_bold[white]%}nix-shell  {$reset_color%}%{$fg[red]%]"
       fi
 
       if git rev-parse --is-inside-work-tree &> /dev/null || [ -n "$IN_NIX_SHELL" ]; then
