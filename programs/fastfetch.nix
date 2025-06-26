b183087cb777d2f2f0653ebe06acac1f14ab1ece
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 20:23:30 +0300

    - Thu 26 Jun 2025 20:23:30 IDT

--- programs/fastfetch.nix
+++ programs/fastfetch.nix
@@ -10,12 +10,52 @@
         };
       };
       display = {
-        size = {
-          binaryPrefix = "si";
-        };
-        color = "blue";
         separator = "  ";
+        color = {
+          keys = "blue";
+          title = "red";
+        };
+        key = {
+          width = 12;
+          type = "string";
+        };
+        bar = {
+          width = 10;
+          charElapsed = "■";
+          charTotal = "-";
+        };
+        percent = {
+          type = 9;
+          color = {
+            green = "green";
+            yellow = "light_yellow";
+            red = "light_red";
+          };
+        };
       };
+      modules = [
+        "title"
+        "separator"
+        {
+          type = "os";
+          key = "OS";
+          keyColor = "blue";
+          format = "{name} {version}";
+        }
+        {
+          type = "kernel";
+          key = "Kernel";
+        }
+        {
+          type = "memory";
+          key = "Memory";
+          percent = {
+            type = 3;
+            green = 30;
+            yellow = 70;
+          };
+        }
+      ];
     };
   };
 }
