eb28baab7569126fcfe82a7af72af5a4a20b9006
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 16:55:48 +0300

    - Thu 26 Jun 2025 16:55:48 IDT

--- programs/git.nix
+++ programs/git.nix
@@ -5,6 +5,8 @@
     userEmail = "shakedgold2005@gmail.com";
     extraConfig.init.defaultBranch = "main";
     extraConfig.push.autoSetupRemote = true;
-    extraConfig.core.excludesFile = "*.nix";
+    ignores = [
+      "*.nix"
+    ];
   };
 }
