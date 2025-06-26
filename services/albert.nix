504839091b44695985e7457404aabc4f04014014
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 21:09:38 +0300

    - Thu 26 Jun 2025 21:09:38 IDT

--- services/albert.nix
+++ services/albert.nix
@@ -15,7 +15,7 @@
       Restart = "always";
       RestartSec = 1;
       ExecStart = pkgs.writeShellScript "albert-env-wrapper.sh" ''
-        export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+        # export PATH="''${XDG_BIN_HOME}:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
         albert
       '';
     };
