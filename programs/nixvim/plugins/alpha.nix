c9f6ef1efde99a74c98b05649082663ef65aaa9c
Shaked Gold <shakedgold2005@gmail.com>
2025-06-27 02:03:08 +0300

    - Fri 27 Jun 2025 02:03:08 IDT

--- programs/nixvim/plugins/alpha.nix
+++ programs/nixvim/plugins/alpha.nix
@@ -2,64 +2,51 @@
   programs.nixvim = {
     plugins.alpha = {
       enable = true;
-      layout = [
-        {
-          type = "padding";
-          val = 2;
-        }
-        {
-          opts = {
-            hl = "Type";
-            position = "center";
-          };
-          type = "text";
-          val = [
-            "                                                                       "
-            "                                                                     "
-            "       ████ ██████           █████      ██                     "
-            "      ███████████             █████                             "
-            "      █████████ ███████████████████ ███   ███████████   "
-            "     █████████  ███    █████████████ █████ ██████████████   "
-            "    █████████ ██████████ █████████ █████ █████ ████ █████   "
-            "  ███████████ ███    ███ █████████ █████ █████ ████ █████  "
-            " ██████  █████████████████████ ████ █████ █████ ████ ██████ "
-            "                                                                       "
-          ];
-        }
-        {
-          type = "padding";
-          val = 2;
-        }
-        {
-          opts = {
-            hl = "Type";
-            position = "center";
-          };
-          type = "group";
-          val = [
-            {
-              on_press = {
-                __raw = "function() vim.cmd[[ene]] end";
-              };
-              opts = {
-                shortcut = "n";
-              };
-              type = "button";
-              val = "  New file";
-            }
-            {
-              on_press = {
-                __raw = "function() vim.cmd[[qa]] end";
-              };
-              opts = {
-                shortcut = "q";
-              };
-              type = "button";
-              val = " Quit Neovim";
-            }
-          ];
-        }
-      ];
+      theme = {
+        __raw = ''
+          local status_ok, alpha = pcall(require, "alpha")
+          if not status_ok then
+           return
+          end
+
+          local dashboard = require("alpha.themes.dashboard")
+          dashboard.section.header.val = {
+
+              [[          ▀████▀▄▄              ▄█ ]],
+              [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+              [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+              [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+              [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+              [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+              [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+              [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+              [[   █   █  █      ▄▄           ▄▀   ]],
+
+          }
+
+           dashboard.section.buttons.val = {
+             dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+             dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+             dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+             dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+             dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
+             dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+          }
+
+          local function footer()
+           return "Don't Stop Until You are Proud..."
+          end
+
+          dashboard.section.footer.val = footer()
+
+          dashboard.section.footer.opts.hl = "Type"
+          dashboard.section.header.opts.hl = "Include"
+          dashboard.section.buttons.opts.hl = "Keyword"
+
+          dashboard.opts.opts.noautocmd = true
+          alpha.setup(dashboard.opts)
+        '';
+      };
     };
   };
 }
