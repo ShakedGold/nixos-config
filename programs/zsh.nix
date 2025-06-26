377bb0f855e6611df76dc64afd0a96418e4ba00b
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 20:44:32 +0300

    - Thu 26 Jun 2025 20:44:32 IDT

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -65,6 +65,8 @@ in {
       [[ "$TERM" == "xterm-kitty" ]] && export TERM=xterm-256color
       ${lib.concatMapStrings (x: "${toString x}\n") (lib.mapAttrsToList (name: value: "export ${name}=${toString value}") config.home.sessionVariables)}
       source ${prompt}
+
+      fastfetch
     '';
 
     oh-my-zsh = {
