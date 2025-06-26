31d7723e8fb6312608f6da9d49af34db716b68dc
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 22:30:23 +0300

    - Thu 26 Jun 2025 22:30:23 IDT

--- programs/nixvim/plugins/telescope.nix
+++ programs/nixvim/plugins/telescope.nix
@@ -1,7 +1,16 @@
 {
-    programs.nixvim = {
-        plugins.telescope = {
-            enable = true;
-        };
+  programs.nixvim = {
+    plugins.telescope = {
+      enable = true;
     };
+
+    keymaps = [
+      {
+        mode = "n",
+        key = "<leader>f";
+        options.silent = true;
+        action = "<cmd>Telescope find_files<CR>";
+      }
+    ];
+  };
 }
