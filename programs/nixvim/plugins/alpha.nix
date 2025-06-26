b74b8c15937b74875fd51eb863a71794fdc4b817
Shaked Gold <shakedgold2005@gmail.com>
2025-06-27 02:14:00 +0300

    - Fri 27 Jun 2025 02:14:00 IDT

--- programs/nixvim/plugins/alpha.nix
+++ programs/nixvim/plugins/alpha.nix
@@ -22,19 +22,13 @@
            }
 
             dashboard.section.buttons.val = {
-              dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+              dashboard.button("f", "󰱼  Find file", ":Telescope find_files <CR>"),
               dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-              dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-              dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-              dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.vim<CR>"),
-              dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+              dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+              dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+              dashboard.button("c", "  Configuration", ":e ~/.config/home-manager/programs/nixvim/default.nix<CR>"),
+              dashboard.button("q", "󰈆  Quit Neovim", ":qa<CR>"),
            }
-
-           local function footer()
-            return "Don't Stop Until You are Proud..."
-           end
-
-           dashboard.section.footer.val = footer()
 
            dashboard.section.footer.opts.hl = "Type"
            dashboard.section.header.opts.hl = "Include"
