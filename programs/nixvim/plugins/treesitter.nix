fd4c1da6aa594fb340b71dba8407af58b2a95118
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 22:38:12 +0300

    - Thu 26 Jun 2025 22:38:12 IDT

--- programs/nixvim/plugins/treesitter.nix
+++ programs/nixvim/plugins/treesitter.nix
@@ -2,6 +2,11 @@
   programs.nixvim = {
     plugins.treesitter = {
       enable = true;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        lua
+        javascript
+        nix
+      ];
     };
   };
 }
