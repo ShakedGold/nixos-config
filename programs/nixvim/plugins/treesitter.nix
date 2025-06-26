f8d56edd7e971152cf89809c262bb9a56da3e203
Shaked Gold <shakedgold2005@gmail.com>
2025-06-26 22:39:01 +0300

    - Thu 26 Jun 2025 22:39:01 IDT

--- programs/nixvim/plugins/treesitter.nix
+++ programs/nixvim/plugins/treesitter.nix
@@ -1,7 +1,14 @@
+{ pkgs, ... }
 {
   programs.nixvim = {
     plugins.treesitter = {
       enable = true;
+
+      settings = {
+        indent = true;
+        highlight = true;
+      };
+
       grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
         lua
         javascript
