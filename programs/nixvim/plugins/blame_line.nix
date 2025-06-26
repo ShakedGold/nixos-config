1eba5b966d7fda39339b54e8298b34db713d3446
Shaked Gold <shakedgold2005@gmail.com>
2025-06-27 01:47:24 +0300

    - Fri 27 Jun 2025 01:47:24 IDT

--- programs/nixvim/plugins/blame_line.nix
+++ programs/nixvim/plugins/blame_line.nix
@@ -8,7 +8,7 @@
           owner = "braxtons12";
           repo = "blame_line.nvim";
           rev = "9b469d97b6cb50b12e5569ddc9d899513d0671b6"; # use latest commit
-          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # use `nix flake show` or `nix-prefetch`
+          sha256 = "sha256-uTZPmYg24oEbp+0kzp7DepL0EdKLevsT9gO/T+UztUI="; # use `nix flake show` or `nix-prefetch`
         };
       })
     ];
