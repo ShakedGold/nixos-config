483affa364dd08259b2f6da51caefd71286cbcbb
Shaked Gold <shakedgold2005@gmail.com>
2025-06-27 02:56:54 +0300

    - Fri 27 Jun 2025 02:56:54 IDT

--- programs/nixvim/options.nix
+++ programs/nixvim/options.nix
@@ -12,11 +12,12 @@
       shiftwidth = 2; # Tab width should be 2
       confirm = true;
       cmdheight = 0;
+
+      undodir = "~/.vim/undo-dir";
+      undofile = true;
     };
 
     clipboard.register = "unnamedplus";
-    undodir = "~/.vim/undo-dir";
-    undofile = true;
 
     globals.mapleader = " ";
   };
