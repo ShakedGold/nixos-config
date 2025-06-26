4deb60ccbcad08b800a09d1702bcdfade8a451ba
Shaked Gold <shakedgold2005@gmail.com>
2025-06-27 01:15:21 +0300

    - Fri 27 Jun 2025 01:15:21 IDT

--- programs/nixvim/plugins/hop.nix
+++ programs/nixvim/plugins/hop.nix
@@ -4,6 +4,18 @@
       enable = true;
 
       settings.keys = "asdghklqwertyuiopzxcvbnmfj";
+      keymaps = [
+        {
+          key = "f";
+          action.__raw = ''
+            function()
+              local hop = require('hop')
+              hop.hint_words()
+            end
+          '';
+          options.remap = true;
+        }
+      ];
     };
   };
 }
