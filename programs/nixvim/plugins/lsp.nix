4d9644e58e2a41715d403f9ca1d1fd4267deaa79
Shaked Gold <shakedgold2005@gmail.com>
2025-06-30 18:06:47 +0300

    - Mon 30 Jun 2025 18:06:47 IDT

--- programs/nixvim/plugins/lsp.nix
+++ programs/nixvim/plugins/lsp.nix
@@ -23,6 +23,58 @@
         clangd.enable = true;
         dockerls.enable = true;
       };
+
+      keymaps = {
+        silent = true;
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
+        };
+      };
     };
 
     plugins.lsp-format.enable = true;
@@ -42,13 +94,18 @@
         }
       )
 
-      vim.diagnostic.config{
+      vim.diagnostic.config {
         float={border=_border}
       };
 
       require('lspconfig.ui.windows').default_options = {
         border = _border
       }
+
+      vim.diagnostic.enable = true
+      vim.diagnostic.config({
+        virtual_lines = true,
+      })
     '';
   };
 }
