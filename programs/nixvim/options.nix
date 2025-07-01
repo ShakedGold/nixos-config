f4fb7e081d3b44d19369710bb833d40689587d71
Shaked Gold <shakedgold2005@gmail.com>
2025-07-01 10:20:12 +0300

    - Tue 01 Jul 2025 10:20:12 IDT

--- programs/nixvim/options.nix
+++ programs/nixvim/options.nix
@@ -6,10 +6,9 @@
       expandtab = true;
       number = true; # Show line numbers
       relativenumber = true; # Show relative line numbers
-      tabstop = 2;
-      softtabstop = 2;
 
-      shiftwidth = 2; # Tab width should be 2
+      smartindent = false;
+
       confirm = true;
       cmdheight = 0;
 
