34a022a3c9b7efbaf03c9b470ed2bb6bbabc3973
Shaked Gold <shakedgold2005@gmail.com>
2025-07-03 22:36:12 +0300

    - Thu 03 Jul 2025 22:36:12 IDT

--- programs/default.nix
+++ programs/default.nix
@@ -1,19 +1,13 @@
 {
   imports = [
-    ./waybar
-    ./rofi
-
     ./zsh.nix
     ./git.nix
     ./ssh.nix
     ./kitty.nix
-    ./hyprland.nix
     ./vscode.nix
     ./plasma.nix
     ./obs-studio.nix
     ./ghostty.nix
-    ./hyprlock.nix
-    ./hyprpaper.nix
     ./tmux.nix
     ./fastfetch.nix
     ./nixvim
