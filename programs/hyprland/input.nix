393f982e75305f61da48bf3c8b6ce3b2aa93a20b
Shaked Gold <shakedgold2005@gmail.com>
2025-07-03 23:32:18 +0300

    - Thu 03 Jul 2025 23:32:18 IDT

--- programs/hyprland/input.nix
+++ programs/hyprland/input.nix
@@ -3,7 +3,9 @@
     kb_layout = "us,il";
     kb_variant = "";
     kb_model = "";
-    kb_options = "caps:none";
+    kb_options = "caps:none,grp:alt_shift_toggle";
     kb_rules = "";
+    sensitivity = 0;
+    force_no_accel = true;
   };
 }
