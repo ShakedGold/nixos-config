d38c673bd705d72d11390368db8f2234ccd6c35b
Shaked Gold <shakedgold2005@gmail.com>
2025-07-03 23:27:13 +0300

    - Thu 03 Jul 2025 23:27:13 IDT

--- programs/hyprland/bind.nix
+++ programs/hyprland/bind.nix
@@ -1,6 +1,13 @@
 {
   wayland.windowManager.hyprland.settings."$mod" = "SUPER";
+
   wayland.windowManager.hyprland.settings.bind = [
     "$mod, F, exec, zen"
+    "$mod, Q, killactive"
+
+    "$mod, H, movefocus, l"
+    "$mod, L, movefocus, r"
+    "$mod, J, movefocus, u"
+    "$mod, K, movefocus, d"
   ];
 }
