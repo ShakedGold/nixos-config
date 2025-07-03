b7205d51a3339256d6321d04deae47be5c3e9c37
Shaked Gold <shakedgold2005@gmail.com>
2025-07-03 23:09:30 +0300

    - Thu 03 Jul 2025 23:09:30 IDT

--- programs/hyprland/monitors.nix
+++ programs/hyprland/monitors.nix
@@ -1,8 +1,8 @@
 {
   wayland.windowManager.hyprland = {
     monitor = [
-      "DP-4, 1920x1080, 0x0, 1.00"
-      "HDMI-A-2, preferred, 0x2560, 1.00"
+      "DP-4, 2560x1440@180, 0x0, 1"
+      "HDMI-A-2, 1920x1080@60, 2560x0, 1, transform, 3"
     ];
   }
 }
