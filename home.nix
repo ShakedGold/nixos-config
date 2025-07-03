6838134fcb1b4f66cd69fc10641e63a5ad0f224a
Shaked Gold <shakedgold2005@gmail.com>
2025-07-03 23:00:35 +0300

    - Thu 03 Jul 2025 23:00:35 IDT

--- home.nix
+++ home.nix
@@ -188,7 +188,7 @@ in {
 
   xdg.portal = {
     enable = true;
-    configPackages = with pkgs; [
+    extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       kdePackages.xdg-desktop-portal-kde
       xdg-desktop-portal-hyprland
