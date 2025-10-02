c0bbed5f0bd40236447ecfd0c1707ba26692d708
Shaked Gold <shakedgold2005@gmail.com>
2025-10-03 00:40:38 +0300

    - Fri 03 Oct 2025 00:40:38 IDT

--- programs/anyrun.nix
+++ programs/anyrun.nix
@@ -2,6 +2,7 @@
   programs.anyrun = {
     enable = true;
     config = {
+      hidePluginInfo = true;
       plugins = [
         "${pkgs.anyrun}/lib/libapplications.so"
         "${pkgs.anyrun}/lib/libsymbols.so"
