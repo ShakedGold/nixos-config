8eacfaad4b099b7802c5d8c1a1edf48fa261f9ee
Shaked Gold <shakedgold2005@gmail.com>
2025-10-03 00:42:01 +0300

    - Fri 03 Oct 2025 00:42:00 IDT

--- programs/anyrun.nix
+++ programs/anyrun.nix
@@ -5,7 +5,9 @@
       hidePluginInfo = true;
       plugins = [
         "${pkgs.anyrun}/lib/libapplications.so"
-        "${pkgs.anyrun}/lib/libsymbols.so"
+        "${pkgs.anyrun}/lib/librink.so"
+        "${pkgs.anyrun}/lib/libkidex.so"
+        "${pkgs.anyrun}/lib/libwebsearch.so"
       ];
     };
   };
