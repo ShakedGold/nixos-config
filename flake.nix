865a313a7d2170c0d2864a09a450f03c7e1b6f27
Shaked Gold <shakedgold2005@gmail.com>
2025-11-07 10:05:54 +0200

    - Fri 07 Nov 2025 10:05:54 IST

--- flake.nix
+++ flake.nix
@@ -28,9 +28,9 @@
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim";
+      # url = "github:nix-community/nixvim";
       # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-25.05";
+      url = "github:nix-community/nixvim/nixos-25.05";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
