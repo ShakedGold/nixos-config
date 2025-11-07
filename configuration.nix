55c529760ed1450122dcd0ec7b33a13b09e28fdd
Shaked Gold <shakedgold2005@gmail.com>
2025-11-07 15:59:01 +0200

    1045 current  2025-11-07 15:57:36  25.05.20251104.ca534a7  6.17.6-zen1                          * - Fri 07 Nov 2025 15:59:01 IST

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,7 @@
 
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", MODE="0666"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="0955", ATTRS{idProduct}=="7321", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0955", ATTRS{idProduct}=="7321", GROUP="plugdev"
   '';
 
   system.autoUpgrade = {
