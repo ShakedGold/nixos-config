dfbeb4f22a8d8d8a588e8f4741ae05db06949fb7
Shaked Gold <shakedgold2005@gmail.com>
2025-12-01 21:30:00 +0200

    1054 current  2025-12-01 20:54:05  25.05.20251124.1c8ba8d  6.17.8-zen1                          * - Mon 01 Dec 2025 21:30:00 IST

--- configuration.nix
+++ configuration.nix
@@ -429,6 +429,7 @@
           repo = "gamescope";
           rev = "5c58b15c0c2d609271151141a1ad7e97911cf10f";
           hash = "sha256-Xh6+wwhqeVM+9eVuHsnXOTOWR04ivxRMZ/SYWYD0gdI=";
+          fetchSubmodules = true;
         };
       });
     })
