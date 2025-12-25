15dce567cd03032db114aca46fe10a498ef182f3
Shaked Gold <shakedgold2005@gmail.com>
2025-12-26 01:42:04 +0200

    1081 current  2025-12-25 16:40:30  25.05.20251124.1c8ba8d  6.17.8-zen1                          * - Fri 26 Dec 2025 01:42:04 IST

--- configuration.nix
+++ configuration.nix
@@ -212,6 +212,10 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  services.vicinae = {
+    packages = pkgs.vicinae;
+  };
+
   programs.zsh.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
